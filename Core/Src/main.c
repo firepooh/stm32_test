5ba933c71f4c2e7732039cc6b708fc6a732b2496
jyp <jypark7@altimobility.com>
2024-07-20 17:15:36 +0900

    -`111

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -311,7 +311,7 @@ void StartDefaultTask(void *argument)
   {
     osDelay(1000);
     HAL_GPIO_TogglePin(LED_GPIO_Port,LED_Pin);
-    printf("test %d : %d.%d.%d \n\r",i++,VERSION_MAJOR,VERSION_MINOR,VERSION_BUGFIX);
+    printf("test %d : %d.%d.%d-%X \n\r",i++,VERSION_MAJOR,VERSION_MINOR,VERSION_BUGFIX,VERSION_SHORT_GIT_HASH);
   }
   /* USER CODE END 5 */
 }
